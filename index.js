4f4a8dcf682d71276cd87ff5a0ac15d9ad386208
riefhanj02 <riefhan8302@gmail.com>
2026-05-22 18:20:19 +0800

    fix errors

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const myVar = process.env.CUSTOM_MESSAGE || "Default setup";
 
-app.get('/', (req, res) => res.send(`<h1>SWE40006 Credit App</h1><p>Message: ${myVar}</p>`));
+app.get('/', (req, res) => res.send(`<h1>SWE40006 Credit App</h1>`));
 app.listen(port, () => console.log('Server running!'));
